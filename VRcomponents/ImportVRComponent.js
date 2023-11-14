df677de47172c4f71fd3feeb618599390bd76190
MichelleLCK <120153122+MichelleLCK@users.noreply.github.com>
2023-11-14 00:45:00 -0800

    Add files via upload

---
+++ VRcomponents/ImportVRComponent.js
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 00:37:26 2023
+
+@author: ALPHAHKU_HTC6
+"""
+
+import { OrbitControls } from '../VRcomponents/OrbitControls.js';
+import { VRButton } from '../VRcomponents/VRButton.js';
+import { XRControllerModelFactory } from '../VRcomponents/XRControllerModelFactory.js';
+import { XRHandModelFactory } from '../VRcomponents/XRHandModelFactory.js';
+
+window.OrbitControls = OrbitControls
+window.VRButton = VRButton
+window.XRControllerModelFactory = XRControllerModelFactory
+window.XRHandModelFactory = XRHandModelFactory
+
+
+
