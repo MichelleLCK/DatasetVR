e83bfcdcbb2196e7efa53d3febef37bc99cb3609
MichelleLCK <120153122+MichelleLCK@users.noreply.github.com>
2023-11-14 21:58:56 +0800

    Update ImportVRComponent.js

--- VRcomponents/ImportVRComponent.js
+++ VRcomponents/ImportVRComponent.js
@@ -1,12 +1,10 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 import { OrbitControls } from '../VRcomponents/OrbitControls.js';
 import { VRButton } from '../VRcomponents/VRButton.js';
 import { XRControllerModelFactory } from '../VRcomponents/XRControllerModelFactory.js';
 import { XRHandModelFactory } from '../VRcomponents/XRHandModelFactory.js';
-
-window.OrbitControls = OrbitControls
-window.VRButton = VRButton
-window.XRControllerModelFactory = XRControllerModelFactory
-window.XRHandModelFactory = XRHandModelFactory
 
 
 
